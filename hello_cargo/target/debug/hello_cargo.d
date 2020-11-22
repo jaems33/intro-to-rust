90b01e095bb4128e9ff9f0640b766cb50b5a11d0
James Teow <james.teow83@gmail.com>
2020-11-21 20:56:24 -0500

    Chapter 1.

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/jamesteow/Desktop/rust/rust_book/hello_cargo/target/debug/hello_cargo: /Users/jamesteow/Desktop/rust/rust_book/hello_cargo/src/main.rs
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-80aea0608def167c.d
@@ -0,0 +1,5 @@
+/Users/jamesteow/Desktop/rust/rust_book/hello_cargo/target/rls/debug/deps/hello_cargo-80aea0608def167c.rmeta: src/main.rs
+
+/Users/jamesteow/Desktop/rust/rust_book/hello_cargo/target/rls/debug/deps/hello_cargo-80aea0608def167c.d: src/main.rs
+
+src/main.rs:
