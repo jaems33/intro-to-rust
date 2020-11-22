b1a3b8c66cb970c64df7afc5c62738d43f703fc0
James Teow <james.teow83@gmail.com>
2020-11-21 21:43:14 -0500

    Finish Chapter 3.1

---
+++ chapter_3/variables/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is {}", x);
+    x = 6;
+    println!("The value of x is {}", x);
+
+    let x = 5;
+    println!("The value of x is {}", x);
+    let x = x + 1;
+    println!("The value of x is {}", x);
+    let x = x * 2;
+    println!("The value of x is {}", x);
+
+    let spaces = "   ";
+    println!("The value of spaces is {}", spaces);
+    let spaces = "Weston Road";
+    println!("The value of spaces is {}", spaces);
+}
